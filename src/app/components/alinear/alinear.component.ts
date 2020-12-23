4c2e33517d6e1b7373e0a9bb69833f2d1c005cfb
Raul Ferrandez <ferrandezraul@gmail.com>
2020-12-23 01:33:34 +0100

    Ongoing dev in alinear view

--- src/app/components/alinear/alinear.component.ts
+++ src/app/components/alinear/alinear.component.ts
@@ -1,39 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/service/api.service';
+import { Jugador } from 'src/app/shared/shared';
 
 @Component({
   selector: 'app-plantillas',
   templateUrl: './alinear.component.html',
   styleUrls: ['./alinear.component.css'],
 })
-export class AlinearComponent {
-    modal_resultado = ""
-    content: Object = null
+export class AlinearComponent implements OnInit {
     nombreJornada = ""
     idJornada = "0"
     plantilla = {}
     nombres_jugadores = []
     selected_jugador: String[] = []
 
-  constructor(private apiService: ApiService) { 
-    for(var i =1; i <= 12; i++) {
-      this.selected_jugador.push("Jugador " + i)
-    }
-    this.init()
-  }
+    porterosPlantilla: Jugador[];
+    defensasPlantilla: Jugador[];
+    mediosPlantilla: Jugador[];
+    delanterosPlantilla: Jugador[];
 
-  async init() {
-    //{"id":"1", "name":"jornada 1"}
+    numberOfDefensas: number[];
+    numberOfMedios: number[];
+    numberOfDelanteros: number[];
+    tacticaForm: FormGroup;
+
+  constructor(private apiService: ApiService,
+              private formBuilder: FormBuilder ) { }
+
+  ngOnInit() {
     this.apiService.getJornadaAlineable().subscribe((response) => {
-      this.nombreJornada = response[0].name
-      this.idJornada = response[0].id
+      console.log("Response de jornada alineable es ", response);
 
-      if (this.idJornada == null) {
-        this.nombreJornada = "NO HAY JORNADA ALINEABLE";
-        return;
-      }
+      // if (response.length == 0) {
+      //   this.nombreJornada = "NO HAY JORNADA ALINEABLE";
+      //   return;
+      // }
+
+      // this.nombreJornada = response[0].name
+      // this.idJornada = response[0].id
+
+      this.apiService.getPlantillaFromCurrentUser().subscribe((jugadores: Jugador[]) => {
+        console.log("Plantilla es ", jugadores);
+        this.readPlantilla(jugadores);
+      });
+
+      return;
 
       this.apiService.getPlantillaFromCurrentUser().subscribe((response: any[]) => {
+        console.log("Plantilla es ", response);
         for (var i = 0; i < response.length; i++) {
           var j = response[i]
           this.plantilla[j["id"]] = j["nombreJugador"]
@@ -42,11 +57,11 @@ export class AlinearComponent {
 
         this.apiService.getAlineacionByJornada(this.idJornada).subscribe((ultima_alineacion: any[]) => {
           if (ultima_alineacion.length == 12) {
-            this.cargarAlineacion(ultima_alineacion)
+            // TODO: this.cargarAlineacion(ultima_alineacion)
           } else {
             this.apiService.getUltimaJornadaTerminada().subscribe((response) => {
               this.apiService.getAlineacionByJornada(response.id.toString()).subscribe((ultima_alineacion_ter: any[]) => {
-                  this.cargarAlineacion(ultima_alineacion_ter)
+                // TODO  this.cargarAlineacion(ultima_alineacion_ter)
               });
             });
           }
@@ -54,33 +69,119 @@ export class AlinearComponent {
       });
 
     });
+
+    this.createTacticaForm();
+
+  } // ngOnInit
+
+  createTacticaForm() {
+    this.tacticaForm = this.formBuilder.group({
+      tacticaSelected: ['3-4-3', Validators.required],
+      porteroSelected: ['', Validators.required],
+      defensaSelected: ['', Validators.required],
+      medioSelected: ['', Validators.required],
+      delanteroSelected: ['', Validators.required],
+    });
+
+    this.onTacticaSelected('3-4-3');
   }
 
-  enviar_alineacion(_content) {
-    this.content = _content
-    var alineacion: String[] = []
-    for(var i =0; i < 12; i++) {
-      alineacion.push(this.getIdJugadorByValue(this.selected_jugador[i]))
-    }
-    this.apiService.sendAlineacion(alineacion, this.idJornada);
+  tacticas() {
+    return ["3-4-3",
+            "3-5-2",
+            "4-3-3",
+            "4-4-2",
+            "4-5-1",
+            "5-3-2",
+            "5-4-1"]
   }
 
-  select_jugador(jugador: string, i: number) {
-    this.selected_jugador[i] = jugador;
+  onTacticaSelected(tactica: string){
+    console.log("Selected tactica", tactica);
+
+    let defensas = tactica.split("-")[0];
+    let medios = tactica.split("-")[1];
+    let delanteros = tactica.split("-")[2];
+
+    this.numberOfDefensas = new Array(parseInt(defensas, 10));
+    this.numberOfMedios = new Array(parseInt(medios, 10));
+    this.numberOfDelanteros = new Array(parseInt(delanteros, 10));
   }
 
-  private getIdJugadorByValue(searchValue: String) {
-    for (let [key, value] of Object.entries(this.plantilla)) {
-      if (value === searchValue)
-        return key;
-    }
+  porterosTactica(){
+    return 1;
   }
 
-  private cargarAlineacion(alineacion: number[]) {
-    if (alineacion.length == 12) {
-      for(var i =0; i < 12; i++) {
-        this.selected_jugador[i] = this.plantilla[alineacion[i]];
+  defensasTactica(){
+    return this.numberOfDefensas;
+  }
+
+  mediosTactica(){
+    return this.numberOfMedios;
+  }
+
+  delanterosTactica(){
+    return this.numberOfDelanteros;
+  }
+
+  porteros(){
+    return this.porterosPlantilla;
+  }
+
+  defensas(){
+    return this.defensasPlantilla;
+  }
+
+  medios(){
+    return this.mediosPlantilla;
+  }
+
+  delanteros(){
+    return this.delanterosPlantilla;
+  }
+
+  onPorteroSelected(portero: string){
+    console.log("Selected portero", portero);
+  }
+
+  onDefensaSelected(defensa: string){
+    console.log("Selected defensa", defensa);
+  }
+
+  onMedioSelected(medio: string){
+    console.log("Selected medio", medio);
+  }
+
+  onDelanteroSelected(delantero: string){
+    console.log("Selected delantero", delantero);
+  }
+
+  readPlantilla(jugadores: Jugador[]){
+    this.porterosPlantilla = [];
+    this.defensasPlantilla = [];
+    this.mediosPlantilla = [];
+    this.delanterosPlantilla = [];
+
+    for( let jugador of jugadores){
+      if(jugador.idDemarcacion == 1){
+        this.porterosPlantilla.push(jugador);
+      } else if( jugador.idDemarcacion == 2){
+        this.defensasPlantilla.push(jugador);
+      } else if( jugador.idDemarcacion == 3){
+        this.mediosPlantilla.push(jugador);
+      } else if( jugador.idDemarcacion == 4){
+        this.delanterosPlantilla.push(jugador);
       }
-    }    
+    }
+
+    console.log("Stored porteros", this.porterosPlantilla);
+    console.log("Stored defensas", this.defensasPlantilla);
+    console.log("Stored medios", this.mediosPlantilla);
+    console.log("Stored delanteros", this.delanterosPlantilla);
   }
+
+  enviarAlineacion() {
+    // console.log("Enviar alineacion con", alineacion);
+  }
+
 }
